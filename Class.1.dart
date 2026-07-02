88cd795f5bc7e7e7cada89b2469bed4e50eed39e
Funsub-Khan <funsubkhan123@gmail.com>
2026-07-02 07:51:58 -0700

    Create Class.1.dart

---
+++ Class.1.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('Hello, World!');
+
+  int num = 70; 
+  int num_1 = 40; 
+  double double_num = 10.5; 
+  bool bool_val = true; 
+  var var_name = 'Learning Dart'; 
+
+// operators
+// arithmetic operators
+  print(num + num_1); 
+  print(num - num_1); 
+  print(num * num_1); 
+  print(num / num_1); 
+  print(num % num_1); 
+}
